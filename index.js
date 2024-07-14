f2ffa5c21c6850742f7e4c7698a459ee9fc6f323
nida-imran173 <101962582+nida-imran173@users.noreply.github.com>
2024-07-14 14:38:25 +0530

    cors option added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -12,10 +12,11 @@ connectDB();
 
 // Init Middleware
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 // Define Routes
-app.use('/api/users', require('./routes/user'));
+app.use("/api/users", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
 
